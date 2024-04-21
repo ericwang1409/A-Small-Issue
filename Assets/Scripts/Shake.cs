36edfa2d1d7735a9aeedadb9d08ffef1fdb99f2f
milosal <milosaldoggy@gmail.com>
2024-04-21 01:43:44 -0400

    calm thdown

--- Assets/Scripts/Shake.cs
+++ Assets/Scripts/Shake.cs
@@ -1,31 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Shake : MonoBehaviour
+public class CameraShaker : MonoBehaviour
 {
-    public bool start = false;
     public AnimationCurve curve;
     public float duration = 1f;
+    public Image fadeImage;
+    private bool isShaking = false;
 
-    void Update() {
-        if (start) {
-            start = false;
+    public void StartShaking()
+    {
+        if (!isShaking)
+        {
             StartCoroutine(Shaking());
+            StartCoroutine(FadeToBlack());
         }
     }
 
-    IEnumerator Shaking() {
-        Vector3 startPosition = transform.position;
+    IEnumerator Shaking()
+    {
+        Vector3 startPosition = transform.localPosition;
         float elapsedTime = 0f;
 
-        while (elapsedTime < duration) {
+        isShaking = true;
+
+        while (elapsedTime < duration)
+        {
             elapsedTime += Time.deltaTime;
             float strength = curve.Evaluate(elapsedTime / duration);
-            transform.position = startPosition + Random.insideUnitSphere * strength;
+            transform.localPosition = startPosition + Random.insideUnitSphere * strength;
             yield return null;
         }
 
-        transform.position = startPosition;
+        transform.localPosition = startPosition;
+        isShaking = false;
+    }
+    IEnumerator FadeToBlack()
+    {
+        float fadeTime = 3f;  
+        float startAlpha = 0f;
+        float endAlpha = 1f;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeTime)
+        {
+            elapsedTime += Time.deltaTime;
+            float newAlpha = Mathf.Lerp(startAlpha, endAlpha, elapsedTime / fadeTime);
+            fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, newAlpha);
+            yield return null;
+        }
+
+        // actions once done fading
     }
 }
+
