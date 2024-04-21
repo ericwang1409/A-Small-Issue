a91276a1bd2dca02d5edde42594ab174f52d6f1e
milosal <milosaldoggy@gmail.com>
2024-04-20 21:26:54 -0400

    rat

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -6,10 +6,46 @@ public class AudioManager : MonoBehaviour
 {
    [SerializeField] AudioSource musicPlayer;
     [SerializeField] AudioSource sfxPlayer;
+    [SerializeField] private List<AudioClip> dayMusicTracks = new List<AudioClip>();
 
-    public static AudioManager j {get; private set; }
-    private void Awake(){
-        j = this;
+    public static AudioManager i {get; private set; }
+    private void Awake() {
+            i = this;
+    }
+
+    void Start() {
+        PlayDailyMusic();
+    }
+
+    public void PlayDailyMusic() {
+        int currentDay = GameController.Instance.day; 
+        Debug.Log("current day:" + currentDay);
+        AudioClip clip = SelectMusicForDay(currentDay);
+        PlayMusic(clip, true);
+    }
+
+    private AudioClip SelectMusicForDay(int day) {
+
+        Debug.Log("select called");
+        if (dayMusicTracks.Count == 0) {
+            Debug.LogWarning("No music tracks are assigned");
+            return null;
+        }
+        
+        if (day == 0 && dayMusicTracks.Count > 0) {
+            Debug.LogWarning("Playing day 1 music");
+            return dayMusicTracks[0];
+        }
+        else if (day == 1 && dayMusicTracks.Count > 1) {
+            Debug.LogWarning("Playing day 2 music");
+            return dayMusicTracks[1];
+        }
+        else if (day >= 2 && dayMusicTracks.Count > 2) {
+            Debug.LogWarning("Playing day 3+ music");
+            return dayMusicTracks[2];
+        }
+
+        return dayMusicTracks[dayMusicTracks.Count - 1]; 
     }
 
     public void PlayMusic(AudioClip clip, bool loop = true) {
